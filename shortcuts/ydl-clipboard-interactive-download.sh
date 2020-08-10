4375464df23f24fd6b5b36b75e93285cb1f70c4a
Türkalp Burak KAYRANCIOĞLU <bkayranci@gmail.com>
2020-08-10 19:23:01 +0300

    clipboard download

---
+++ shortcuts/ydl-clipboard-interactive-download.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+DOWNLOAD_DIR="${HOME}/storage/downloads"
+
+link="$(termux-clipboard-get)"
+
+if [ -z "$link" ]
+then
+  termux-toast "Link is not found."
+  exit 1
+fi
+
+youtube-dl -F "${link}"
+
+echo "Choose a media type:"
+read -r media_type
+
+if [ -z "$media_type" ]
+then
+  termux-toast "media_type is not found."
+  exit 1
+fi
+
+youtube-dl -f "${media_type}" "${link}" -o "${DOWNLOAD_DIR}/%(title)s.%(ext)s"
